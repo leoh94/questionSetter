89e34cf3d4f5bb09a4114d41e442e442ae7e9e31
Leo Hussain <leo.94@hotmail.co.uk>
2018-05-05 05:46:39 -0400

    elite

--- uceshus/platforms/browser/www/js/uploadData.js
+++ uceshus/platforms/browser/www/js/uploadData.js
@@ -8,14 +8,18 @@ function startDataUpload() {
 	alert(name + " "+ surname + " "+module);
 	
 	var question = document.getElementByID("question").value;
+	
+	alert(question);
+	
 	var answer1 = document.getElementByID("answer1").value;
 	var answer2 = document.getElementByID("answer2").value;
 	var answer3 = document.getElementByID("answer3").value;
 	var answer4 = document.getElementByID("answer4").value;
 	
+	alert(answer1 + " "+answer2 + " "+answer3 + " "+answer4)
 	
 	var postString = "name="+name +"&surname="+surname+"&module="+module;
-	postString = postString +"&question="+question+"&answer1="+answer1+"&answer2="+answer2+"&answer3="+answer3+"&answer4="+answer4;
+	postString = postString + "&question="+question+"&answer1="+answer1+"&answer2="+answer2+"&answer3="+answer3+"&answer4="+answer4;
 	// now get the geometry values
 	var latitude = document.getElementById("latitude").value;
 	var longitude = document.getElementById("longitude").value;
